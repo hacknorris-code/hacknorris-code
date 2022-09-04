b7e30dc7b603072a1f25eafabcf2684a6204b776
Neo <hacknorris@tutanota.com>
2022-09-04 17:27:33 +0200

    update of evil file, hehe

--- corps-i-hate.s
+++ corps-i-hate.s
@@ -1,15 +1,24 @@
 global _start
 section .data 
+a0 db "i will add here even more later. now its only this : ", 10
 a db "google", 10
 b db "facebook", 10
 c db "apple", 10
 d db "steam", 10
 e db "netflix", 10
 f db "microsoft", 10
+g db "twitter", 10
+h db "gamejolt", 10
+i db "onet", 10
+j db "wp", 10
 
 section .text
 
 _start:
+
+mov rdx, 54
+mov rsi, a0
+call print 
 
 mov rdx, 7
 mov rsi, a
@@ -34,6 +43,22 @@ call print
 mov rdx, 10
 mov rsi, f
 call print
+
+mov rdx, 8
+mov rsi, g 
+call print 
+
+mov rdx, 9
+mov rsi, h
+call print 
+
+mov rdx, 5
+mov rsi, i 
+call print 
+
+mov rdx, 3
+mov rsi, j 
+call print 
 
 mov rax, 60
 xor rdi, rdi
